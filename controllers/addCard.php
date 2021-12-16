3213d42ef97d27c8722a7c8033348bd59a39839c
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-12-16 14:54:26 +0100

    maj

--- controllers/addCard.php
+++ controllers/addCard.php
@@ -2,7 +2,6 @@
   $capture = new capture();
   $cards = new cards();
   if (isset($_POST['addCard'])) {
-      if (isset($_FILES['giveCard']) && !empty($_FILES['giveCard'])) {
                 $cards->pathName = $_POST['cardPath'];
                 $cards->name = $_POST['cardName'];
                 $cards->nb = $_POST['cardNumber'];
@@ -10,6 +9,5 @@
               ?>
               <meta http-equiv="refresh" content="1;URL=/accueil ?>">
               <?php
-      }
   }
 ?>
