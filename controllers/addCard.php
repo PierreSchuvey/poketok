003dc6f7002b94a647de0878fef8c0ca2d2d4f01
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-12-16 14:40:55 +0100

    maj

--- controllers/addCard.php
+++ controllers/addCard.php
@@ -3,12 +3,8 @@
   $cards = new cards();
   if (isset($_POST['addCard'])) {
       if (isset($_FILES['giveCard']) && !empty($_FILES['giveCard'])) {
-              $uploaddir = '/assets/images/Cards/';
-              $uploadfile = $uploaddir . basename($_FILES['giveCard']['name']);
-              if(move_uploaded_file($_FILES['giveCard']['tmp_name'], $uploadfile)){
                 $cards->pathName = $_FILES['giveCard']['name'];
                 $cards->addCards();
-              }
               ?>
               <meta http-equiv="refresh" content="1;URL=/accueil ?>">
               <?php
